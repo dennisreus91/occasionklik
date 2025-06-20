e36540616375a8389726da080652dfff8860947a
dennisreus91 <dennisreus91@gmail.com>
2025-06-20 15:13:37 +0200

    Fix Playwright install for Render

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "📦 Start build: Installing Playwright and dependencies"
 
-pip install playwright
+echo "📦 Start build: Installing Playwright and dependencies"
+pip install --upgrade pip
+pip install -r requirements.txt
 playwright install --with-deps
